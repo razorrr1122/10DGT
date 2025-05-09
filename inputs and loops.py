0e3fd82e44b4041f4918ff056b0383ffc12cdefd
razorrr1122 <joy.kim@kingsway.school.nz>
2025-05-09 14:12:56 +1200

    Add files via upload
    Version 2 of inputs and loops. Added some more functionally to the program. Added multiplication and print formatting.

--- inputs and loops.py
+++ inputs and loops.py
@@ -6,6 +6,7 @@
 # Ask the user a question and store theitr answer in a variable
 # Ask the user for their name and store it
 name = input("Kia ora, what is your name?") # Store answer as a string
+print() # This adds a line break 
 
 # Ask the user for two numbers and then add them together
 num1 = int(input("What is your first number?"))
@@ -15,9 +16,15 @@ print(f"You entered your second number as {num2}")
 
 # Adding the two answers together
 sum = num1 + num2
-print(sum)
+print(f"The two numbeers added together will print as {sum}")
+
+# Multiplying the two inputs together
+multiply = num1 * num2
+print(f"The two numbers multiplied together will print as {multiply}")
+
 
 # Test that input was stored correctly
-print(name)
+print(f"Nice to meet you, {name}")
 # to comment code out , use ctrl + /
+
 
